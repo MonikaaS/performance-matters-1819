c2f5f78f3631fdcd71aa13773cac154c4a7596dc
Monika <monika_streljajeva@hotmail.com>
2019-03-12 16:58:02 +0100

    git init

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const fetch = require('node-fetch');
+
+app.use(express.static('public'))
+
+//app.get('/', (req, res) => res.send(`<h1>Hello world!</h1>`))
+//app.get('/:id', (req, res) => res.send(req.params))
+
+app.set('view engine', 'ejs')
+
+app.get('/', function (req, res) {
+    fetch(
+            'https://www.anapioficeandfire.com/api/houses?pageSize=50'
+        )
+        .then(res => res.json())
+        .then(data =>
+            res.render("pages/index", {
+                data: data
+            })
+        )
+        .catch(error => console.error('Error:', error));
+});
+
+app.get('/:id', function (req, res) {
+    const id = req.params.id
+
+    fetch(
+            'https://www.anapioficeandfire.com/api/houses?&name=' + id
+        )
+        .then(res => res.json())
+        .then(data =>
+            res.render("pages/detail", {
+                data: data
+            })
+        )
+        .catch(error => console.error('Error:', error));
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}`))
