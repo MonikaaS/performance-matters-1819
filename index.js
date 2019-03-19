c0202582a2ec86b1d04ac4c1aeb4ac57425ca217
Monika <monika_streljajeva@hotmail.com>
2019-03-19 16:26:56 +0100

    Changed vars to const

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const fetch = require("node-fetch");
-var bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 
 app.use(express.static("public"));
 
