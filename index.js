89aeb6da90644bc5134ebfd55234011d3e7e0781
Monika <monika_streljajeva@hotmail.com>
2019-03-21 14:56:47 +0100

    Add new font and image

--- index.js
+++ index.js
@@ -3,19 +3,20 @@ const app = express();
 const port = 3000;
 const fetch = require("node-fetch");
 const bodyParser = require("body-parser");
+var compression = require('compression');
 
+// app.use(compression())
 app.use(express.static("public"));
-
-// middleware for bodyParser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 //app.get('/', (req, res) => res.send(`<h1>Hello world!</h1>`))
 //app.get('/:id', (req, res) => res.send(req.params))
 
 app.set("view engine", "ejs");
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   fetch("https://www.anapioficeandfire.com/api/houses?pageSize=50")
     .then(res => res.json())
     .then(data =>
@@ -26,7 +27,7 @@ app.get("/", function(req, res) {
     .catch(error => console.error("Error:", error));
 });
 
-app.get("/:id", function(req, res) {
+app.get("/:id", function (req, res) {
   const id = req.params.id;
 
   fetch("https://www.anapioficeandfire.com/api/houses?&name=" + id)
@@ -39,7 +40,7 @@ app.get("/:id", function(req, res) {
     .catch(error => console.error("Error:", error));
 });
 
-app.post("/search", function(req, res) {
+app.post("/search", function (req, res) {
   var item = req.body.userSearchInput;
   console.log(item);
 
